c18aa4a0d33cb6a5d850e47fe614502b1978215a
J-Lw <JLaw0@proton.me>
2024-06-09 13:37:11 -0300

    Solved p624.

--- algorithms/src/max_distance_array.rs
+++ algorithms/src/max_distance_array.rs
@@ -2,8 +2,24 @@
 // https://leetcode.com/problems/maximum-distance-in-arrays/description/
 
 fn max_distance(arrays: Vec<Vec<i32>>) -> i32 {
-    
-}
+    let mut min_n = arrays[0][0];
+    let mut max_n = arrays[0][0];
+
+    for array in &arrays {
+        let first_element = array[0];
+        let last_element = array[array.len() - 1];
+
+        if first_element < min_n {
+            min_n = first_element;
+        }
+
+        if last_element > max_n {
+            max_n = last_element;
+        }
+    }
+
+    max_n - min_n
+}   
 
 #[cfg(test)]
 mod tests {
