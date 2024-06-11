33419796c162902ba1ebbc889aea21881b29e94d
J-Lw <JLaw0@proton.me>
2024-06-11 18:02:34 -0300

    Checkpoint.

--- algorithms/src/wiggle_sort.rs
+++ algorithms/src/wiggle_sort.rs
@@ -2,6 +2,16 @@
 // https://leetcode.com/problems/wiggle-sort/description/
 
 fn wiggle_sort(numbers: &mut Vec<i32>) {
+    numbers.sort();
+    let mut product = Vec::new();
+
+    for (index, number) in numbers.iter().enumerate() { 
+        product.insert(number);
+        product.push(sorted_numbers[index + 2]);
+        product.push(sorted_numbers[index + 1]);
+    }
+
+    product
 }   
 
 #[cfg(test)]
@@ -10,8 +20,8 @@ mod tests {
 
     #[test]
     fn standard_case() {
-        let input = ;
-        let target_product = ;
-        assert_eq!(wiggle_sort(input), target_product);
+        let input: Vec<i32> = vec![3, 5, 2, 1, 6, 4];
+        let target_product = vec![3, 5, 1, 6, 2, 4];
+        assert_eq!(wiggle_sort(&input), target_product);
     }
 }
