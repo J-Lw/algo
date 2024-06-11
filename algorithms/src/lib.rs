2db46646056faad7cbbbf353305dc344a208e4bb
J-Lw <JLaw0@proton.me>
2024-06-11 16:46:30 -0300

    Wiggle sort init.

--- algorithms/src/lib.rs
+++ algorithms/src/lib.rs
@@ -1,1 +1,2 @@
 mod max_distance_array;
+mod wiggle_sort;
---
+++ algorithms/src/wiggle_sort.rs
@@ -0,0 +1,17 @@
+// Leetcode p280 - Wiggle Sort.
+// https://leetcode.com/problems/wiggle-sort/description/
+
+fn wiggle_sort(numbers: &mut Vec<i32>) {
+}   
+
+#[cfg(test)]
+mod tests {
+    use super::wiggle_sort;
+
+    #[test]
+    fn standard_case() {
+        let input = ;
+        let target_product = ;
+        assert_eq!(wiggle_sort(input), target_product);
+    }
+}
